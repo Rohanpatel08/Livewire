c47f7fc673d3d879b5de5123cd13877056e46dc0
Rohanpatel08 <raj.bangoriya08@gmail.com>
2025-02-25 22:51:57 +0530

    changes in decreament function

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -8,25 +8,26 @@ class Counter extends Component
 {
 
     public $count = 0;
-    public $title = 'Counter';
+    public $message = '';
+
 
     public function increment()
     {
-        if ($this->count == 0) {
-            return "You can't decrease more";
-        } else {
-            $this->count++;
-        }
+        $this->count++;
     }
 
     public function decreament()
     {
-        $this->count--;
+        if ($this->count == 0) {
+            $this->message = "You can't decrease more";
+        } else {
+            $this->count--;
+        }
     }
     public function render()
     {
-
+        $title = 'Counter';
         return view('livewire.counter')
-            ->layout('components.layouts.app', ['title' => $this->title]);
+            ->layout('components.layouts.app', ['title' => $title, 'message' => $this->message]);
     }
 }
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,9 +1,10 @@
 <div>
 
     <div style="text-align: center;">
-        <h1>Hello Rohan</h1>
+        <h1>Hello World</h1>
         <button wire:click="increment" class="btn btn-success">+</button>
         <button wire:click="decreament" class="btn btn-danger">-</button>
         <h3>{{ $count }}</h3>
+        <span class="mt-2 text-danger">{{$message}}</span>
     </div>
 </div>
